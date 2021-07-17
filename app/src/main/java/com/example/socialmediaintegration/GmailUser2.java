ba40a2c528346749e9f736570d252e92aabb00ff
Sabarna Ghosh <s123456789ghosh@gmail.com>
2021-07-18 02:16:17 +0530

    This is my first commit

---
+++ app/src/main/java/com/example/socialmediaintegration/GmailUser2.java
@@ -0,0 +1,9 @@
+package com.example.socialmediaintegration;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.common.ConnectionResult;
+
+public interface GmailUser2 {
+    void onConnectionFailed(@NonNull ConnectionResult connectionResult);
+}
